859a16bb991f69838465ffee962d3c0ce841aaa6
cristiancobra <cobra@empresadigital.net.br>
2020-06-04 20:01:38 -0300

    LINK temporario do wordpress

--- app/Http/Controllers/SiteCliente.php
+++ app/Http/Controllers/SiteCliente.php
@@ -10,22 +10,19 @@ class SiteCliente extends Controller {
     /**
      * Gera o link do site do cliente no Menu
      */
-    
     public function EditarSite() {
         $url_cliente = Auth::user()->dominio;
-        
-        return view('editarsite', [
-                'url_cliente' => $url_cliente
-            ]);
-               
-    }
-    
+        return Redirect('https://' . $url_cliente . '/wp-admin/');
+            }
+
     public function PostarSite() {
         $url_cliente = Auth::user()->dominio;
-        return Redirect('https://'.$url_cliente.'/wp-admin/post-new.php');
-    }       
+        return Redirect('https://' . $url_cliente . '/wp-admin/post-new.php');
     }
-         /**
+
+}
+
+/**
         return Redirect('https://'.$url_cliente.'/wp-admin');
           * 
           * 
