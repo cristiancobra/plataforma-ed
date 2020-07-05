4341c3c47323ed612fea03dff8a69f43777324c6
cobra <cobra@note-cobra>
2020-07-05 18:48:24 -0300

    correcao do valor oportunidades

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,9 +6,7 @@ use Illuminate\Http\Request;
 use Auth;
 use App\Lead;
 use App\Task;
-use App\UserCrm;
 use App\Opportunities;
-use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller {
 
@@ -29,9 +27,11 @@ class HomeController extends Controller {
 	public function index() {
 		$user = Auth::user();
 		$user_crm = Auth::user()->idcrm;
-		$tarefas_abertas = Task::where('status', 'Not Started' AND 'assigned_user_id', $user_crm)
-		->get();
-
+		$tarefas_abertas = Task::where([
+					['status', 'Not Started'],
+					['assigned_user_id', $user_crm]
+				])
+				->get();
 
 		$potenciais_abertos = Lead::where([
 					['converted', '0'],
@@ -47,7 +47,7 @@ class HomeController extends Controller {
 
 		$total_tarefas = count($tarefas_abertas);
 		$total_potenciais = count($potenciais_abertos);
-		$valor_oportunidades = count($oportunidades_abertas);
+		$valor_oportunidades = $oportunidades_abertas->sum('amount');
 
 		if ($user->perfil == "administrador") {
 
