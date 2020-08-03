40f3df76bb1c876cac737efb25a2fe752938dbc6
cobra <cobra@note-cobra>
2020-08-03 15:30:59 -0300

    correção na migration chagne column in email

--- database/migrations/2020_07_21_211051_change_column_user_id_in_emails.php
+++ database/migrations/2020_07_21_211051_change_column_user_id_in_emails.php
@@ -13,7 +13,7 @@ class ChangeColumnUserIdInEmails extends Migration
      */
     public function up()
     {
-        Schema::table('users', function (Blueprint $table) {
+        Schema::table('emails', function (Blueprint $table) {
             $table->foreignId('user_id')->constrained();
         });
     }
@@ -25,7 +25,7 @@ class ChangeColumnUserIdInEmails extends Migration
      */
     public function down()
     {
-        Schema::table('users', function (Blueprint $table) {
+        Schema::table('emails', function (Blueprint $table) {
             $table->integer('user_id')->required();
         });
     }
