b56fd676f08097061b5f9b57f5453e2350783db7
cobra <cobra@note-cobra>
2020-08-27 15:59:28 -0300

    visualizacao do ckeditor em showtask

--- resources/views/tasks/showTask.blade.php
+++ resources/views/tasks/showTask.blade.php
@@ -13,24 +13,49 @@
 @section('main')
 <br>
 <div style="padding-left: 6%">
-	<h1 class="name"> {{ $task->name }}  </h1>
-	<p class="labels">CATEGORIA:<span class="fields">{{ $task->category }} </span></p>
-	<p class="labels">RESPONSÁVEL:<span class="fields">{{ $task->users->name }} </span></p>
+	<h1 class="name">
+		{{ $task->name }}
+	</h1>
+	<p class="labels">
+		CATEGORIA:
+		<span class="fields">{{ $task->category }} </span></p>
+	<p class="labels">
+		RESPONSÁVEL:<span class="fields">{{ $task->users->name }} </span>
+	</p>
 	<br>
-	<p class="labels">DATA DE CRIAÇÃO:<span class="fields">  {{ $task->date_start }} </span></p>
-	<p class="labels">PRAZO FINAL:<span class="fields">  {{ $task->date_due }} </span></p>
+	<p class="labels">
+		DATA DE CRIAÇÃO:<span class="fields">  {{ $task->date_start }} </span>
+	</p>
+	<p class="labels">
+		PRAZO FINAL:<span class="fields">  {{ $task->date_due }} </span>
+	</p>
 	<br>
-	<p class="labels">DESCRIÇÃO:<span class="fields">  {{ $task->description }} </span></p>
-	<p class="labels">CONTATO:<span class="fields">  {{ $task->contact_Id }} </span></p>
-	<p class="labels">PRIORIDADE:<span class="fields">  {{ $task->priority }} </span></p>
+	<p class="labels">
+		DESCRIÇÃO:<span class="fields">  	{!!html_entity_decode($task->description)!!} </span>
+	</p>
+	<p class="labels">
+		CONTATO:<span class="fields">  {{ $task->contact_Id }} </span>
+	</p>
+	<p class="labels">
+		PRIORIDADE:<span class="fields">  {{ $task->priority }} </span>
+	</p>
 	<br>
-	<p class="labels">INÍCIO:<span class="fields">  {{ $task->start_time }} </span></p>
-	<p class="labels">TÉRMINO:<span class="fields">  {{ $task->end_time }} </span></p>
-	<p class="labels">DURAÇÃO:<span class="fields">  {{ $task->duration }} hora(s)</span></p>
+	<p class="labels">
+		INÍCIO:<span class="fields">  {{ $task->start_time }} </span>
+	</p>
+	<p class="labels">
+		TÉRMINO:<span class="fields">  {{ $task->end_time }} </span>
+	</p>
+	<p class="labels">
+		DURAÇÃO:<span class="fields">  {{ $task->duration }} hora(s)</span>
+	</p>
 	<br>
-	<p class="labels">SITUAÇAO:<span class="fields">  {{ $task->status }} </span></p>
+	<p class="labels">
+		SITUAÇAO:<span class="fields">  {{ $task->status }} </span>
+	</p>
 	<br>
-	<p class="fields">Criado em:  {{ date('d/m/Y H:i', strtotime($task->created_at)) }} </p>
+	<p class="fields">Criado em:  {{ date('d/m/Y H:i', strtotime($task->created_at)) }}
+	</p>
 
 	<div style="text-align:right;padding: 2%">
 		<form   style="text-decoration: none;color: black;display: inline-block" action="{{ route('task.destroy', ['task' => $task->id]) }}" method="post">
