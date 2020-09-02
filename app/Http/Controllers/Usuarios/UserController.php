dcde132ccd6594e90f7a2c6e2d1320e541ce5866
cobra <cobra@note-cobra>
2020-09-02 17:50:19 -0300

    correção paginate USERS 3

--- app/Http/Controllers/Usuarios/UserController.php
+++ app/Http/Controllers/Usuarios/UserController.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Auth;
 class UserController extends Controller {
 
 	/**
-	 * Display a listing of the resource.
+	 * Display a listing of the resource
 	 *
 	 * @return \Illuminate\Http\Response
 	 */
