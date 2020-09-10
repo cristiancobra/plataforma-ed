a2d6f65995208b837c57afea5d76badb7bfe2e78
cobra <cobra@note-cobra>
2020-09-10 10:49:08 -0300

    migration com bug

--- database/migrations/2020_09_03_185314_change_columns_nullable_in_accounts.php
+++ database/migrations/2020_09_03_185314_change_columns_nullable_in_accounts.php
@@ -4,41 +4,42 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class ChangeColumnsNullableInAccounts extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::table('accounts', function (Blueprint $table) {
-	$table->string('email')->nullable()->change();
-	$table->string('phone')->nullable()->change();
-	$table->string('site')->nullable()->change();
-	$table->string('address')->nullable()->change();
-	$table->string('type')->nullable()->change();
-	$table->smallInteger('employees')->nullable()->change();
-	$table->text('description')->nullable()->change();
-        });
-    }
+class ChangeColumnsNullableInAccounts extends Migration {
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::table('accounts', function (Blueprint $table) {
-	$table->string('email')->change();
-	$table->string('phone')->change();
-	$table->string('site')->change();
-	$table->string('address')->change();
-	$table->string('type')->change();
-	$table->tinyInteger('employees')->change();
-	$table->text('description')->change();
-        });
-    }
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::table('accounts', function (Blueprint $table) {
+			$table->string('email')->nullable()->change();
+			$table->string('phone')->nullable()->change();
+			$table->string('site')->nullable()->change();
+			$table->string('address')->nullable()->change();
+			$table->string('type')->nullable()->change();
+			$table->smallInteger('employees')->nullable()->change();
+			$table->text('description')->nullable()->change();
+			$table->string('status');  // criar campo STATUS
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::table('accounts', function (Blueprint $table) {
+			$table->dropColumn('status');  // apaga campo STATUS
+			$table->string('email')->change();
+			$table->string('phone')->change();
+			$table->string('site')->change();
+			$table->string('address')->change();
+			$table->string('type')->change();
+			$table->smallInteger('employees')->change();
+			$table->text('description')->change();
+		});
+	}
+
 }
