c35695a79f907090ef1d443e89327be0361be808
cobra <cobra@note-cobra>
2020-10-08 16:06:43 -0300

    Exibição das horas trabalhadas hoje 6 - adicao de agosto

--- resources/views/usuarios/dashboardUser.blade.php
+++ resources/views/usuarios/dashboardUser.blade.php
@@ -48,7 +48,7 @@
 
     <div class="hours">
 		<p class="numeros_painel" style="color: yellow">
-			{{ $hoursToday / 3600 }}
+			{{ number_format($hoursToday / 3600, 1, ',','.')  }} horas
 		</p>
 		<p class="subtitulo-branco">
 			horas concluidas hoje
