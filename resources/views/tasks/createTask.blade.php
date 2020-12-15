0eea72898ec34d46993212347ff3029e2612a0dc
cobra <cobra@note-cobra>
2020-12-15 09:40:34 -0300

    correção: comentários das questoes

--- resources/views/tasks/createTask.blade.php
+++ resources/views/tasks/createTask.blade.php
@@ -58,7 +58,7 @@
 			</option>
 			@foreach ($users as $user)
 			<option  class="fields" value="{{ $user->id }}">
-				{{ $user->name }}
+				{{ $user->contactc->name }}
 			</option>
 			@endforeach
 		</select>
