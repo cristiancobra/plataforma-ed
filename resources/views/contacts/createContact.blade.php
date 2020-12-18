a851f95131788a0399e79ef0a931448b4336e972
cobra <cobra@note-cobra>
2020-12-18 10:54:28 -0300

    finalizacao do contrato 5

--- resources/views/contacts/createContact.blade.php
+++ resources/views/contacts/createContact.blade.php
@@ -1,205 +1,247 @@
 @extends('layouts/master')
 
-@section('title','CONTRATOS')
+@section('title','NOVO CONTATO')
 
 @section('image-top')
-{{ asset('imagens/contract.png') }} 
+{{ asset('imagens/contact.png') }} 
 @endsection
-
 
 @section('description')
 @endsection
 
 @section('buttons')
-<a class="button-primary"  href="{{route('contact.create')}}">
+<a class="button-primary"  href="{{route('contact.index')}}">
 	VOLTAR
 </a>
 @endsection
 
 @section('main')
+<br>
+
+@if(Session::has('failed'))
+<div class="alert alert-danger">
+	{{ Session::get('failed') }}
+	@php
+	Session::forget('failed');
+	@endphp
+</div>
+@endif
 <div>
-    <form action=" {{ route('contract.store') }} " method="post" style="padding: 40px;color: #874983">
-        @csrf
-        <label class="labels" for="" >EMPRESA: </label>
-        <select name="account_id">
-            @foreach ($accounts as $account)
-            <option  class="fields" value="{{$account->id}}">
-                {{$account->name}}
-            </option>
-            @endforeach
-        </select>
-        <br>
-        <br>
-        <label class="labels" for="" >OPORTUNIDADE:</label>
-        <select name="opportunitie">
-            @foreach ($opportunities as $opportunitie)
-            <option  class="fields" value="{{$opportunitie->id}}">
-                {{$opportunitie->name}}
-            </option>
-            @endforeach
-        </select>
-        <br>
-        <br>
-        <label class="labels" for="" >CONTATO: </label>
-        <select name="contact_id">
-            @foreach ($contacts as $contact)
-            <option  class="fields" value="{{ $contact->id }}">
-                {{ $contact->name }}
-            </option>
-            @endforeach
-        </select>
-        <br>
-        <label class="labels" for="" >PRIMEIRA TESTEMUNHA: </label>
-        <select name="witness1">
-            @foreach ($contacts as $contact)
-            <option  class="fields" value="{{ $contact->id }}">
-                {{ $contact->name }}
-            </option>
-            @endforeach
-        </select>
-        <br>
-        <label class="labels" for="" >SEGUNDA TESTEMUNHA: </label>
-        <select name="witness2">
-            @foreach ($contacts as $contact)
-            <option  class="fields" value="{{ $contact->id }}">
-                {{ $contact->name }}
-            </option>
-            @endforeach
-        </select>
-        <br>
-        <br>
-        <label class="labels" for="" >DATA DE INICIO:</label>
-        <input type="date" name="date_start" size="20"><span class="fields"></span>
-        <br>
-        <label class="labels" for="" >DATA DA ENTREGA FINAL:</label>
-        <input type="date" name="date_conclusion" size="20"><span class="fields"></span>
-        <br>
-        <label class="labels" for="" >DATA DO PAGAMENTO:</label>
-        <input type="date" name="pay_day" size="20"><span class="fields"></span>
-        <br>
-             <br>
-        <label class="labels" for="" >OBRIGAÇÕES DA EMPRESA:</label>
->
-        <textarea id="accountObrigation" name="accountObrigation" rows="20" cols="90">
-		{{ $contract->observations }}
-        </textarea>
-<!------------------------------------------- SCRIPT CKEDITOR---------------------- -->
-        <script src="//cdn.ckeditor.com/4.5.7/standard/ckeditor.js"></script>
-        <script>
-CKEDITOR.replace('accountObrigation');
-        </script>
-        <br>
-        <br>
-            <br>
-        <label class="labels" for="" >OBRIGAÇÕES DO CLIENTE:</label>
-        <textarea id="contactObrigation" name="contactObrigation" rows="20" cols="90">
-		{{ $contract->contactObrigation }}
-        </textarea>
-                   <!------------------------------------------- SCRIPT CKEDITOR---------------------- -->
-        <script src="//cdn.ckeditor.com/4.5.7/standard/ckeditor.js"></script>
-        <script>
-CKEDITOR.replace('contactObrigation');
-        </script>
-        <br>
-        
-        <label class="labels" for="" >OBSERVAÇÕES:</label>
-        <textarea id="observations" name="observations" rows="20" cols="90">
-		{{ $contract->observations }}
-        </textarea>
-        <!------------------------------------------- SCRIPT CKEDITOR---------------------- -->
-        <script src="//cdn.ckeditor.com/4.5.7/standard/ckeditor.js"></script>
-        <script>
-CKEDITOR.replace('observations');
-        </script>
-        <br>
-    <label class="labels" for="" >PRODUTOS:</label>
-<br>
-<table class="table-list">
-	<tr>
-		<td   class="table-list-header" style="width: 5%">
-			<b>QTDE
-			</b></td>
-		<td   class="table-list-header" style="width: 55%">
-			<b>NOME</b>
-		</td>
-		<td   class="table-list-header" style="width: 10%">
-			<b>PRAZO</b>
-		</td>
-		<td   class="table-list-header" style="width: 10%">
-			<b>IMPOSTO </b>
-		</td>
-		<td   class="table-list-header" style="width: 10%">
-			<b>UNITÁRIO</b>
-		</td>
-		<td   class="table-list-header" style="width: 10%">
-			<b>TOTAL</b>
-		</td>
-	</tr>
+	<form action=" {{ route('contact.store') }} " method="post" style="color: #874983">
+		@csrf
+		<label for="" >DONO: </label>
+		<select name="account_id">
+			@foreach ($accounts as $account)
+			<option  class="fields" value="{{ $account->id }}">
+				{{ $account->name }}
+			</option>
+			@endforeach
+		</select>
+		<br>
+		<label for="">Origem do Lead: </label>
+		<select name="lead_source">
+			<option  class="fields" value="site">
+				site
+			</option>
+			<option  class="fields" value="pesquisa paga">
+				pesquisa paga
+			</option>
+			<option  class="fields" value="pesquisa orgânica">
+				pesquisa orgânica
+			</option>
+			<option  class="fields" value="email marketing">
+				email marketing
+			</option>
+			<option  class="fields" value="indicação">
+				indicação
+			</option>
+			<option  class="fields" value="mídia social">
+				mídia social
+			</option>
+			<option  class="fields" value="outbound">
+				outbound
+			</option>
+			<option  class="fields" value="desconhecida">
+				desconhecida
+			</option>
+			<option  class="fields" value="outros">
+				outros
+			</option>
+		</select>
+		<br>
+		<br>
+		<br>
+		<h2 class="name" for="">PESSOAL</h2>
+		<label for="" >Primeiro nome: </label>
+		<input type="text" name="first_name" value="{{old('first_name')}}">
+		@if ($errors->has('first_name'))
+		<span class="text-danger">{{ $errors->first('first_name') }}</span>
+		@endif
+		<br>
+		<label for="" >Sobrenome: </label>
+		<input type="text" name="last_name" value="{{old('last_name')}}">
+		@if ($errors->has('last_name'))
+		<span class="text-danger">{{ $errors->first('last_name') }}</span>
+		@endif
+		<br>
+		<label for="" >Data de Nascimento: </label>
+		<input type="date" name="date_birth">
+		<br>
+		<label for="" >CPF: </label>
+		<input type="text" name="cpf">
+		<br>
+		<br>
+		<br>
 
-	@foreach ($invoiceLines as $invoiceLine)
-	<tr style="font-size: 14px">
-		<td class="table-list-center">
-			{{$invoiceLine->amount}}
-		</td>
-		<td class="table-list-left">
-			{{$invoiceLine->product->name}}
-		</td>
-		<td class="table-list-center">
-			{{$invoiceLine->subtotalDeadline}} dia(s)
-		</td>
-		<td class="table-list-right">
-			{{ number_format($invoiceLine->subtotalTax_rate, 2,",",".") }}
-		</td>
+		<h2 class="name" for="">PROFISSIONAL</h2>
+		<label for="">Profissão: </label>
+		<input type="text" name="profession">   
+		<br>
+		<label for="">Empresa: </label>
+		<input type="text" name="company">   
+		<br>
+		<label for="">Cargo: </label>
+		<input type="text" name="job_position">   
+		<br>
+		<label for="">Escolaridade: </label>
+		<select name="schollarity">
+			<option  class="fields" value="">
+			</option>
+			<option  class="fields" value="fundamental">
+				ensino fundamental
+			</option>
+			<option  class="fields" value="médio">
+				ensino médio
+			</option>
+			<option  class="fields" value="superior incompleto">
+				superior incompleto
+			</option>
+			<option  class="fields" value="superior completo">
+				superior completo
+			</option>
+			<option  class="fields" value="pós-graduação">
+				pós-graduação
+			</option>
+			<option  class="fields" value="sem escolaridade">
+				sem escolaridade
+			</option>
+		</select>
+		<br>
+		<br>
+		<br>
 
-		<td class="table-list-right">
-			{{ number_format($invoiceLine->product->price,2,",",".") }}
-		</td>
+		<h2 class="name" for="">CONTATOS</h2>
+		<label for="" >Email: </label>
+		<input type="text" name="email" value="{{old('email')}}">
+		@if ($errors->has('email'))
+		<span class="text-danger">{{ $errors->first('email') }}</span>
+		@endif
+		<br>
+		<label for="">Telefone: </label>
+		<input type="text" name="phone">   
+		<br>
+		<label for="">Site: </label>
+		<input type="text" name="site">   
+		<br>
+		<label for="">Instagram: </label>
+		<input type="text" name="instagram">   
+		<br>
+		<label for="">Facebook: </label>
+		<input type="text" name="facebook">   
+		<br>
+		<label for="">Linkedin: </label>
+		<input type="text" name="linkedin">   
+		<br>
+		<label for="">Twitter: </label>
+		<input type="text" name="twitter">   
+		<br>
+		<br>
+		<br>
 
-		<td class="table-list-right">
-			{{ number_format($invoiceLine->subtotalPrice,2,",",".") }}
-		</td>
-	</tr>
+		<h2 class="name" for="">LOCALIZAÇÃO</h2>
+		<label for="">Endereço: </label>
+		<input type="text" name="address">   
+		<br>
+		<label for="city">Cidade: </label>
+		<input type="text" name="city">   
+		<br>
+		<label for="">Bairro: </label>
+		<input type="text" name="neighborhood">   
+		<br>
+		<label for="">Estado: </label>
+		<select name="state">
+			{{createSelect($states)}}
+		</select>
+		<br>
+		<br>
+		<label for="">País: </label>
+		<input type="text" name="country" value="Brasil">   
+		<br>
+		<br>
+		<br>
 
-	<tr style="font-size: 12px">
-		<td class="table-list-left" colspan="5">
-			{!!html_entity_decode($invoiceLine->product->description)!!}
-		</td>
-	</tr>
-	@endforeach
+		<h2 class="name" for="">PERFIL</h2>
+		<p>Utilize esses dados apenas com permissão dos contatos e se você for importante para seu modelo de negócio, obedecendo o previsto na
+			<a href="http://www.planalto.gov.br/ccivil_03/_ato2015-2018/2018/lei/L13709.htm">
+				Lei Geral de Proteção de Dados
+			</a>.
+			<br>
+			<br>	
+			<label for="">Estado Civil: </label>
+			<select name="civil_state">
+				<option  class="fields" value="solteiro">
+					solteiro(a)
+				</option>
+				<option  class="fields" value="casado">
+					casado(a)
+				</option>
+				<option  class="fields" value="divorciado">
+					divorciado(a)
+				</option>
+				<option  class="fields" value="união estável">
+					união estável
+				</option>
+				<option  class="fields" value="viúvo">
+					viúvo(a)
+				</option>
+			</select>
 
-	<tr>
-		<td   class="table-list-header-right" colspan="4">
-		</td>
-		<td   class="table-list-header-right">
-			desconto: 
-		</td>
-		<td   class="table-list-header-right">
-			<b>- {{number_format($invoice->discount, 2,",",".") }}</b>
-		</td>
-	</tr>
-	<tr>
-		<td   class="table-list-header-right" colspan="4">
-			
-		<td   class="table-list-header-right">
-			TOTAL: 
-		</td>
-		</td>
-		<td   class="table-list-header-right">
-			<b>R$ {{number_format($invoice->totalPrice, 2,",",".") }}</b>
-		</td>
-	</tr>
-</table>
-<br>
-        <br>
-        <label class="labels" for="">SITUAÇÃO:</label>
-        <select class="fields" name="status">
-            <option value="pending">pendente</option>
-            <option value="disable">desativado</option>
-            <option value="active">ativo</option>
-        </select>
-        <br>
-        <br>
-        <input class="btn btn-secondary" type="submit" value="CRIAR CONTRATO">
-    </form>
-</div>     
+			<br>
+			<label for="">Naturalidade: </label>
+			<input type="text" name="naturality">
+			<br>
+			<label for="">Filhos: </label>
+			<input type="number" name="kids">   
+			<br>
+			<label for="">Hobbie: </label>
+			<input type="text" name="hobbie">   
+			<br>
+			<label for="">Renda: </label>
+			<input type="text" name="income">   
+			<br>
+			<label for="">Religião: </label>
+			<input type="text" name="religion">   
+			<br>
+			<label for="">Etinia: </label>
+			<input type="text" name="etinicity">   
+			<br>
+			<label for="">Orientação Sexual: </label>
+			<input type="text" name="sexual_orientation">   
+			<br>
+			<br>
+			<label for="">Tipo: </label>
+			<input type="text" name="type">
+			<br>
+			<br>
+			<label for="status">SITUAÇÃO: </label>
+			<select class="fields" name="status">
+				<option value="ativo">ativo</option>
+				<option value="pendente">pendente</option>
+				<option value="desativado">desativado</option>
+			</select>
+			<br>
+			<br>
+			<input class="btn btn-secondary" type="submit" value="CRIAR">
+	</form>
+</div>
 @endsection
