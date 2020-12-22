6f0c1911a33b034ae2108df2f3f61e2bfb5504f6
cobra <cobra@note-cobra>
2020-12-22 17:30:52 -0300

    correção que impedia de abrir o edit

--- resources/views/sales/opportunities/editOpportunity.blade.php
+++ resources/views/sales/opportunities/editOpportunity.blade.php
@@ -25,19 +25,19 @@
 </div>
 @endif
 <div>
-	<form action=" {{ route('opportunitie.update', ['opportunitie' =>$opportunitie->id]) }} " method="post" style="padding: 40px;color: #874983">
+	<form action=" {{ route('opportunity.update', ['opportunity' =>$opportunity->id]) }} " method="post" style="padding: 40px;color: #874983">
 		@csrf
 		@method('put')
 		<label class="labels" for="" >NOME:</label>
-		<input type="text" name="name" size="20" value="{{$opportunitie->name}}"><span class="fields"></span>
+		<input type="text" name="name" size="20" value="{{$opportunity->name}}"><span class="fields"></span>
 		@if ($errors->has('name'))
 		<span class="text-danger">{{ $errors->first('name') }}</span>
 		@endif
 		<br>
 		<label class="labels" for="" >DONO: </label>
 		<select name="account_id">
-			<option  class="fields" value="{{ $opportunitie->account->id }}">
-				{{ $opportunitie->account->name }}
+			<option  class="fields" value="{{ $opportunity->account->id }}">
+				{{ $opportunity->account->name }}
 			</option>
 			@foreach ($accounts as $account)
 			<option  class="fields" value="{{ $account->id }}">
@@ -49,8 +49,8 @@
 		<br>
 		<label class="labels" for="" >CONTATO: </label>
 		<select name="contact_id">
-			<option  class="fields" value="{{ $opportunitie->contact_id }}">
-				{{ $opportunitie->contact->name }}
+			<option  class="fields" value="{{ $opportunity->contact_id }}">
+				{{ $opportunity->contact->name }}
 			</option>
 			@foreach ($contacts as $contact)
 			<option  class="fields" value="{{ $contact->id }}">
@@ -60,12 +60,12 @@
 		</select>
 		<br>
 		<label class="labels" for="" >DATA DE CRIAÇÃO:</label>
-		<input type="date" name="date_start" size="20" value="{{$opportunitie->date_start}}"><span class="fields"></span>
+		<input type="date" name="date_start" size="20" value="{{$opportunity->date_start}}"><span class="fields"></span>
 		<br>
 		<br>
 		<label class="labels" for="" >DESCRIÇÃO:</label>
 		<textarea id="description" name="description" rows="20" cols="90">
-		{{ $opportunitie->description }}
+		{{ $opportunity->description }}
 		</textarea>
 		<!------------------------------------------- SCRIPT CKEDITOR---------------------- -->
 		<script src="//cdn.ckeditor.com/4.5.7/standard/ckeditor.js"></script>
@@ -77,7 +77,7 @@ CKEDITOR.replace('description');
 		<br>
 		<label class="labels" for="">ETAPA DA VENDA:</label>
 		<select class="fields" name="stage">
-			<option value="{{$opportunitie->stage}}">{{$opportunitie->stage}}</option>
+			<option value="{{$opportunity->stage}}">{{$opportunity->stage}}</option>
 			<option value="prospecção">prospecção</option>
 			<option value="apresentação">apresentação</option>
 			<option value="proposta">proposta</option>
@@ -86,12 +86,12 @@ CKEDITOR.replace('description');
 		</select>
 		<br>
 		<label class="labels" for="" >PRÓXIMO CONTATO:</label>
-		<input type="date" name="date_conclusion" size="20" value="{{$opportunitie->date_conclusion}}"><span class="fields"></span>
+		<input type="date" name="date_conclusion" size="20" value="{{$opportunity->date_conclusion}}"><span class="fields"></span>
 		<br>
 		<br>
 		<label class="labels" for="">SITUAÇÃO:</label>
 		<select class="fields" name="status">
-			<option value="{{$opportunitie->status}}">{{$opportunitie->status}}</option>
+			<option value="{{$opportunity->status}}">{{$opportunity->status}}</option>
 			<option value="pendente">pendente</option>
 			<option value="fazendo agora">fazendo agora</option>
 			<option value="cancelada">cancelada</option>
@@ -101,13 +101,13 @@ CKEDITOR.replace('description');
 		<br>
 		<div style="text-align: right">
 			<input class="btn btn-secondary" style="display:inline-block" type="submit" value="SALVAR">	
-			<a class="btn btn-secondary" href=" {{route('opportunitie.index')}} "  style="text-decoration: none;color: white;display: inline-block">
+			<a class="btn btn-secondary" href=" {{route('opportunity.index')}} "  style="text-decoration: none;color: white;display: inline-block">
 				<i class='fas fa-arrow-alt-circle-left'></i>  VOLTAR
 			</a>
 		</div>
 	</form>
 	<div style="text-align:right">
-		<form   style="text-decoration: none;display: inline-block" action="{{ route('opportunitie.destroy', ['opportunitie' => $opportunitie->id]) }}" method="post">
+		<form   style="text-decoration: none;display: inline-block" action="{{ route('opportunity.destroy', ['opportunity' => $opportunity->id]) }}" method="post">
 			@csrf
 			@method('delete')
 			<input class="btn btn-danger" type="submit" value="APAGAR">
