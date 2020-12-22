c0c87c09414fe4c8f078728058a49a61eafc777e
cobra <cobra@note-cobra>
2020-12-22 16:25:22 -0300

    link não abrirá mais em outro janela

--- resources/views/sales/opportunities/createOpportunity.blade.php
+++ resources/views/sales/opportunities/createOpportunity.blade.php
@@ -50,7 +50,9 @@
 			</option>
 			@endforeach
 		</select>
-		<a class="btn btn-secondary" href="{{ route('contact.create') }}" target="_blank">NOVO CONTATO</a>
+		<a class="btn btn-secondary" href="{{ route('contact.create') }}">
+			NOVO CONTATO
+		</a>
 		<br>
 		<br>
 		<label class="labels" for="" >DATA DE CRIAÇÃO:</label>
