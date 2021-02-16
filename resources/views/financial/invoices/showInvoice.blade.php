b9088f84a9f90ec7077214d477f43cd5d86a9b8e
cobra <cobra@note-cobra>
2021-02-16 16:04:29 -0300

    erro na fatura

--- resources/views/financial/invoices/showInvoice.blade.php
+++ resources/views/financial/invoices/showInvoice.blade.php
@@ -81,10 +81,10 @@ Sem contrato
 </button>
 @endif
 <br>
-<label class="labels" for="" >CONTATO:</label>
 @if($invoice->type == 'receita')
-<span class="fields">{{$invoice->contact->name}}</span>
-<a href="{{route('contact.show', ['contact' => $invoice->contact_id])}}">
+<label class="labels" for="" >CONTATO:</label>
+<span class="fields">{{$invoice->opportunity->contact->name}}</span>
+<a href="{{route('contact.show', ['contact' => $invoice->opportunity->contact_id])}}">
 	<i class='fa fa-eye' style="color:white"></i>
 </a>
 @endif
