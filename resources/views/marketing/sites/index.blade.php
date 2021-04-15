300c4f733c262b61176aa658e3493bdc9c167f1e
cobra <cobra@cobra-desktop>
2021-04-15 19:45:17 -0300

    Correção em editar que apagava a oportunidade

--- resources/views/marketing/sites/index.blade.php
+++ resources/views/marketing/sites/index.blade.php
@@ -3,83 +3,91 @@
 @section('title','SITES')
 
 @section('image-top')
-{{ asset('imagens/site.png') }} 
+{{asset('imagens/site.png')}} 
 @endsection
 
 @section('description')
 @endsection
 
 @section('buttons')
-<a class="circular-button primary"  href="{{route('site.create')}}">
-	NOVO SITE
+<a class='circular-button primary'  href='{{route('site.create')}}'>
+    <i class='fa fa-list' aria-hidden='true'></i>
 </a>
-<a class="circular-button primary"  href="{{route('domain.index')}}">
-	DOMÍNIOS
+<a class='circular-button primary'  href='{{route('domain.index')}}'>
+    <i class='fa fa-plus' aria-hidden='true'></i>
 </a>
 @endsection
 
 @section('main')
 <div>
-	<p class="subtitulo-roxo" style="text-align: right;padding-top: 2%;padding-right: 6%">
-		Você possui <span class="labels">{{$totalSites }} produtos </span>
-	</p>
-	<br>
-	<table class="table-list">
-		<tr>
-			<td   class="table-list-header"><b>NOME </b></td>
-			<td   class="table-list-header"><b>DOMÍNIOS </b></td>
-			<td   class="table-list-header"><b>LINKS</b></td>
-			<td   class="table-list-header"><b>SITUAÇÃO</b></td>
-		</tr>
+    <p class='subtitulo-roxo' style='text-align: right;padding-top: 2%;padding-right: 6%'>
+        Você possui <span class='labels'>{{$totalSites }} produtos </span>
+    </p>
+    <br>
+    <table class='table-list'>
+        <tr>
+            <td class='table-list-header'>
+                NOME
+            </td>
+            <td class='table-list-header'>
+                DOMÍNIOS
+            </td>
+            <td class='table-list-header'>
+                LINKS
+            </td>
+            <td class='table-list-header'>
+                SITUAÇÃO
+            </td>
+        </tr>
 
-		@foreach ($sites as $site)
-		<tr style="font-size: 14px">
-			<td class="table-list-left">
-				<button class="button-round">
-					<a href="{{route('site.show', ['site' => $site->id])}}">
-						<i class='fa fa-eye' style="color:white"></i></a>
-				</button>
-				<button class="button-round">
-					<a href="{{route('site.edit', ['site' => $site->id])}}">
-						<i class='fa fa-edit' style="color:white"></i></a>
-				</button>
-				{{$site->name}}
-			</td>
+        @foreach ($sites as $site)
+        <tr style='font-size: 14px'>
+            <td class='table-list-left'>
+                <button class='button-round'>
+                    <a href='{{route('site.show', ['site' => $site->id])}}'>
+                        <i class='fa fa-eye' style='color:white'></i></a>
+                </button>
+                <button class='button-round'>
+                    <a href='{{route('site.edit', ['site' => $site->id])}}'>
+                        <i class='fa fa-edit' style='color:white'></i></a>
+                </button>
+                {{$site->name}}
+            </td>
 
-			<td class="table-list-left">
-				@foreach ($site->domains as $domain)
-				<a class="white"  href="{{route('domain.show', ['domain' => $domain->id])}}">
-					<button class="button-round">
-						<i class='fa fa-eye'></i>
-					</button> 
-				</a>
-				{{$domain->name}}</li>
-				<br>
-				</a>
-				@endforeach
-			</td>
+            <td class='table-list-left'>
+                @foreach ($site->domains as $domain)
+                <a class='white'  href='{{route('domain.show', ['domain' => $domain->id])}}'>
+                    <button class='button-round'>
+                        <i class='fa fa-eye'></i>
+                    </button> 
+                </a>
+                {{$domain->name}}</li>
+                <br>
+                </a>
+                @endforeach
+            </td>
 
-			<td class="table-list-center">
-				<button class="button-round">
-					<a href="//{{$site->link_view}}" target="_blank">
-						<i class='fa fa-eye' style="color:white"></i></a>
-				</button>
-				<button class="button-round">
-					<a href="//{{$site->link_edit}}"  target="_blank">
-						<i class='fa fa-edit' style="color:white"></i></a>
-				</button>
-				<button class="button-round">
-					<a href="//{{$site->link_hosting}}"  target="_blank">
-						<i class='fa fa-server' style="color:white"></i></a>
-				</button>
-			</td>
-			{{formatStatusActive($site)}}
-		</tr>
-		@endforeach
-	</table>
-	<p style="text-align: right">
-		<br>
-		{{ $sites->links() }}
-	</p>
-	<br>
-	@endsection
+            <td class='table-list-center'>
+                <button class='button-round'>
+                    <a href='//{{$site->link_view}}' target='_blank'>
+                        <i class='fa fa-eye' style='color:white'></i></a>
+                </button>
+                <button class='button-round'>
+                    <a href='//{{$site->link_edit}}'  target='_blank'>
+                        <i class='fa fa-edit' style='color:white'></i></a>
+                </button>
+                <button class='button-round'>
+                    <a href='//{{$site->link_hosting}}'  target='_blank'>
+                        <i class='fa fa-server' style='color:white'></i></a>
+                </button>
+            </td>
+            {{formatStatusActive($site)}}
+        </tr>
+        @endforeach
+    </table>
+    <p style='text-align: right'>
+        <br>
+        {{ $sites->links() }}
+    </p>
+    <br>
+    @endsection
--- resources/views/operational/journey/indexJourneys.blade.php
+++ resources/views/operational/journey/indexJourneys.blade.php
@@ -11,97 +11,97 @@
 
 @section('buttons')
 <a class="circular-button primary"  href="{{route('journey.create')}}">
-	<i class="fa fa-plus" aria-hidden="true"></i>
+    <i class="fa fa-plus" aria-hidden="true"></i>
 </a>
 @endsection
 
 @section('main')
 <div style="text-align:right">
-	<form action=" {{route('journey.index')}} " method="post" style="color: #874983;display: inline-block">
-		@csrf
-		<select class="select"name="user_id">
-			<option  class="fields" value="">
-				funcionário
-			</option>
-			<option  class="fields" value="">
-				TODOS
-			</option>
-			@foreach ($users as $user)
-			<option  class="fields" value="{{$user->id}}">
-				{{$user->name}}
-			</option>
-			@endforeach
-		</select>
-		<input class="btn btn-secondary" type="submit" value="FILTRAR">
-	</form>
+    <form action=" {{route('journey.index')}} " method="post" style="color: #874983;display: inline-block">
+        @csrf
+        <select class="select"name="user_id">
+            <option  class="fields" value="">
+                funcionário
+            </option>
+            <option  class="fields" value="">
+                TODOS
+            </option>
+            @foreach ($users as $user)
+            <option  class="fields" value="{{$user->id}}">
+                {{$user->name}}
+            </option>
+            @endforeach
+        </select>
+        <input class="btn btn-secondary" type="submit" value="FILTRAR">
+    </form>
 </div>
 <table class="table-list">
-	<tr>
-		<td   class="table-list-header" style="width: 20%">
-			<b>DATA </b>
-		</td>
-		<td   class="table-list-header" style="width: 35%">
-			<b>TAREFA </b>
-		</td>
-		<td   class="table-list-header" style="width: 15%">
-			<b>RESPONSÁVEL </b>
-		</td>
-		<td   class="table-list-header" style="width: 5%">
-			<b>INÍCIO </b>
-		</td>
-		<td   class="table-list-header" style="width: 5%">
-			<b>TÉRMINO </b>
-		</td>
-		<td   class="table-list-header" style="width: 5%">
-			<b>DURAÇÃO </b>
-		</td>
-	</tr>
+    <tr>
+        <td   class="table-list-header" style="width: 20%">
+            <b>DATA </b>
+        </td>
+        <td   class="table-list-header" style="width: 35%">
+            <b>TAREFA </b>
+        </td>
+        <td   class="table-list-header" style="width: 15%">
+            <b>RESPONSÁVEL </b>
+        </td>
+        <td   class="table-list-header" style="width: 5%">
+            <b>INÍCIO </b>
+        </td>
+        <td   class="table-list-header" style="width: 5%">
+            <b>TÉRMINO </b>
+        </td>
+        <td   class="table-list-header" style="width: 5%">
+            <b>DURAÇÃO </b>
+        </td>
+    </tr>
 
-	@foreach ($journeys as $journey)
-	<tr style="font-size: 14px">
-		<td class="table-list-left">
-			<button class="button-round">
-				<a href=" {{ route('journey.show', ['journey' => $journey]) }}">
-					<i class='fa fa-eye' style="color:white"></i></a>
-			</button>
-			<button class="button-round">
-				<a href=" {{ route('journey.edit', ['journey' => $journey]) }}">
-					<i class='fa fa-edit' style="color:white"></i></a>
-			</button>
-			{{date('d/m/Y', strtotime($journey->date))}}
-		</td>
-		<td class="table-list-left">
-			<button class="button-round">
-				<a href=" {{ route('task.show', ['task' => $journey->task_id]) }}">
-					<i class='fa fa-eye' style="color:white"></i></a>
-			</button>
-			@isset($journey->task)
-			{{$journey->task->name}}
-			@endisset
+    @foreach ($journeys as $journey)
+    <tr style="font-size: 14px">
+        <td class="table-list-left">
+            <button class="button-round">
+                <a href=" {{ route('journey.show', ['journey' => $journey]) }}">
+                    <i class='fa fa-eye' style="color:white"></i></a>
+            </button>
+            <button class="button-round">
+                <a href=" {{ route('journey.edit', ['journey' => $journey]) }}">
+                    <i class='fa fa-edit' style="color:white"></i></a>
+            </button>
+            {{date('d/m/Y', strtotime($journey->date))}}
+        </td>
+        <td class="table-list-left">
+            <button class="button-round">
+                <a href=" {{ route('task.show', ['task' => $journey->task_id]) }}">
+                    <i class='fa fa-eye' style="color:white"></i></a>
+            </button>
+            @isset($journey->task)
+            {{$journey->task->name}}
+            @endisset
 
-		</td>
-		<td class="table-list-center">
-			{{$journey->user->contact->name}}
-		</td>
-		<td class="table-list-center">
-			{{date('H:i', strtotime($journey->start_time))}}
-		</td>
-		<td class="table-list-center">
-			@if($journey->end_time == null)
-			--
-			@else
-			{{date('H:i', strtotime($journey->end_time))}}
-			@endif
-		</td>
-		<td class="table-list-center" style="color:white;background-color: #874983">
-			{{ gmdate('H:i', $journey->duration) }}
-		</td>
-	</tr>
-	@endforeach
+        </td>
+        <td class="table-list-center">
+            {{$journey->user->contact->name}}
+        </td>
+        <td class="table-list-center">
+            {{date('H:i', strtotime($journey->start_time))}}
+        </td>
+        <td class="table-list-center">
+            @if($journey->end_time == null)
+            --
+            @else
+            {{date('H:i', strtotime($journey->end_time))}}
+            @endif
+        </td>
+        <td class="table-list-center" style="color:white;background-color: #874983">
+            {{ gmdate('H:i', $journey->duration) }}
+        </td>
+    </tr>
+    @endforeach
 </table>
 <p style="text-align: right">
-	<br>
-	{{ $journeys->links() }}
+    <br>
+    {{ $journeys->links() }}
 </p>
 <br>
 @endsection
