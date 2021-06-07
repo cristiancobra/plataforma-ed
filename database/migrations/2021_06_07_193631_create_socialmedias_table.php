418a40d68dc84847cf0043264ed2278920b930cf
cobra <cobra@cobra-desktop>
2021-06-07 19:48:30 -0300

    Novas correçoes em relatórios de marketing

---
+++ database/migrations/2021_06_07_193631_create_socialmedias_table.php
@@ -0,0 +1,93 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSocialmediasTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('socialmedias', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('account_id');
+            $table->foreignId('company_id')->nullable();
+            $table->string('socialmedia_name');
+            $table->string('name');
+            $table->string('URL_name');
+            $table->string('socialmedia_phone')->nullable();
+            $table->string('socialmedia_email')->nullable();
+            $table->string('URL_studio')->nullable();
+            $table->tinyInteger('business')->nullable();
+            $table->tinyInteger('linked_instagram')->nullable();
+            $table->tinyInteger('linked_facebook')->nullable();
+            $table->tinyInteger('same_site_name')->nullable();
+            $table->tinyInteger('about')->nullable();
+            $table->tinyInteger('feed_content')->nullable();
+            $table->tinyInteger('harmonic_feed')->nullable();
+            $table->tinyInteger('SEO_descriptions')->nullable();
+            $table->tinyInteger('feed_images')->nullable();
+            $table->tinyInteger('stories')->nullable();
+            $table->tinyInteger('interaction')->nullable();
+            $table->tinyInteger('igtv')->nullable();
+            $table->tinyInteger('reels')->nullable();
+            $table->tinyInteger('employee_profiles')->nullable();
+            $table->tinyInteger('employee_profiles_cv')->nullable();
+            $table->tinyInteger('offers_job')->nullable();
+            $table->tinyInteger('pin_content')->nullable();
+            $table->decimal('value_ads', 8, 2)->nullable();
+            $table->tinyInteger('linktree')->nullable();
+            $table->tinyInteger('image_banner')->nullable();
+            $table->tinyInteger('organized_playlists')->nullable();
+            $table->tinyInteger('liked_virtualstore')->nullable();
+            $table->tinyInteger('video_banner')->nullable();
+            $table->tinyInteger('legend')->nullable();
+            $table->tinyInteger('feed_member')->nullable();
+            $table->tinyInteger('follow_channel')->nullable();
+            $table->integer('followers');
+            $table->integer('male_13_17');
+            $table->integer('male_18_24');
+            $table->integer('male_25_34');
+            $table->integer('male_35_44');
+            $table->integer('male_45_54');
+            $table->integer('male_55_65');
+            $table->integer('male_65');
+            $table->integer('female_13_17');
+            $table->integer('female_18_24');
+            $table->integer('female_25_34');
+            $table->integer('female_35_44');
+            $table->integer('female_45_54');
+            $table->integer('female_55_65');
+            $table->integer('female_65');
+            $table->string('city_followers_1');
+            $table->integer('number_city_followers_1');
+            $table->string('city_followers_2');
+            $table->integer('number_city_followers_2');
+            $table->string('city_followers_3');
+            $table->integer('number_city_followers_3');
+            $table->string('observation')->nullable();
+            $table->string('keyword_1')->nullable();
+            $table->string('keyword_2')->nullable();
+            $table->string('keyword_3')->nullable();
+            $table->string('keyword_4')->nullable();
+            $table->string('keyword_5')->nullable();
+            $table->string('type');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('socialmedias');
+    }
+
+}
--- resources/views/marketing/socialmedia/index.blade.php
+++ resources/views/marketing/socialmedia/index.blade.php
@@ -13,89 +13,71 @@ Total: <span class="labels">{{$total}}</span>
 @endsection
 
 @section('buttons')
-<a class="button-primary"  href="{{route('socialmedia.create',[
+<a class="circular-button secondary"  href="{{route('socialmedia.create',[
 	'type' => 'minha',
 ])}}">
-	CRIAR
+    <i class="fa fa-plus" aria-hidden="true"></i>
 </a>
+{{createButtonBack()}}
+{{createButtonList('socialmedia')}}
 @endsection
 
 @section('main')
-<form action="{{route('socialmedia.index')}} " method="post" style="text-align: right;color: #874983">
-	@csrf
-	<input type="text" name="name" placeholder="nome da rede social" value="">
-	<select class="select" name="account_id">
-		<option  class="select" value="">
-			Qualquer empresa
-		</option>
-		@foreach ($accounts as $account)
-		<option  class="select" value="{{$account->id}}">
-			{{$account->name}}
-		</option>
-		@endforeach
-		<option  class="select" value="">
-			todas
-		</option>
-	</select>
-	{{createFilterSelect('socialmedia_name', 'select', returnSocialmediaType())}}
-	<input class="btn btn-secondary" type="submit" value="FILTRAR">
-</form>
-<br>
 <table class="table-list">
-	<tr>
-		<td   class="table-list-header">
-			NOME DA REDE SOCIAL 
-		</td>
-		<td   class="table-list-header">
-			URL  
-		</td>
-		<td   class="table-list-header">
-			STUDIO DE CRIAÇÃO
-		</td>
-		<td   class="table-list-header">
-			SITUAÇÃO
-		</td>
-	</tr>
+    <tr>
+        <td   class="table-list-header">
+            NOME DA REDE SOCIAL 
+        </td>
+        <td   class="table-list-header">
+            URL  
+        </td>
+        <td   class="table-list-header">
+            STUDIO DE CRIAÇÃO
+        </td>
+        <td   class="table-list-header">
+            SITUAÇÃO
+        </td>
+    </tr>
 
-	@foreach ($socialmedias as $socialmedia)
-	<tr style="font-size: 14px">
-		<td class="table-list-left">
-			<button class="button-round">
-				<a href=" {{ route('socialmedia.show', ['socialmedia' => $socialmedia->id]) }}">
-					<i class='fa fa-eye' style="color:white"></i>
-				</a>
-			</button>
-			<button class="button-round">
-				<a href=" {{ route('socialmedia.edit', ['socialmedia' => $socialmedia->id]) }}">
-					<i class='fa fa-edit' style="color:white"></i>
-				</a>
-			</button>
-			
-			{{$socialmedia->socialmedia_name}}
-		</td>
-		<td class="table-list-center">
-			{{$socialmedia->URL_name}} 
-			<button class="button-round">
-				<a href=" {{$socialmedia->URL_name}}">
-					<i class='fa fa-share-alt' style="color:white"></i>
-				</a>
-		</td>
-		<td class="table-list-center">
-			
-			</button>
-			<button class="button-round">
-				<a href=" {{ $socialmedia->URL_studio }}">
-					<i class='fa fa-video' style="color:white"></i>
-				</a>
-			</button>
-		</td>
-		{{formatsocialmediaStatus($socialmedia)}}
-	</tr>
-	@endforeach
+    @foreach ($socialmedias as $socialmedia)
+    <tr style="font-size: 14px">
+        <td class="table-list-left">
+            <button class="button-round">
+                <a href=" {{ route('socialmedia.show', ['socialmedia' => $socialmedia->id]) }}">
+                    <i class='fa fa-eye' style="color:white"></i>
+                </a>
+            </button>
+            <button class="button-round">
+                <a href=" {{ route('socialmedia.edit', ['socialmedia' => $socialmedia->id]) }}">
+                    <i class='fa fa-edit' style="color:white"></i>
+                </a>
+            </button>
+
+            {{$socialmedia->socialmedia_name}}
+        </td>
+        <td class="table-list-center">
+            {{$socialmedia->URL_name}} 
+            <button class="button-round">
+                <a href=" {{$socialmedia->URL_name}}">
+                    <i class='fa fa-share-alt' style="color:white"></i>
+                </a>
+        </td>
+        <td class="table-list-center">
+
+            </button>
+            <button class="button-round">
+                <a href=" {{ $socialmedia->URL_studio }}">
+                    <i class='fa fa-video' style="color:white"></i>
+                </a>
+            </button>
+        </td>
+        {{formatsocialmediaStatus($socialmedia)}}
+    </tr>
+    @endforeach
 </table>
 <p style="text-align: right">
-	<br>
-	{{$socialmedias->links()}}
+    <br>
+    {{$socialmedias->links()}}
 </p>
 <br>
 @endsection
