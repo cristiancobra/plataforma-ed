c5c878327e46b32743b7dd43fd58e69cf1a909bb
cobra <cobra@cobra-desktop>
2021-06-12 12:48:45 -0300

    finalizacao do crud de questions

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -8,37 +8,35 @@ use App\Models\User;
 
 class Report extends Model {
 
-	protected $table = 'reports';
-	protected $fillable = [
-		'id',
-		'user_id',
-		'account_id',
-		'name', 'date',
-		'status', 
-		'general', 
-		'logo', 
-		'pallet',
-	];
+    protected $table = 'reports';
+    protected $fillable = [
+        'id',
+        'user_id',
+        'account_id',
+        'name',
+        'date',
+        'status',
+        'general',
+    ];
 
-	public function account() {
-		return $this->belongsTo(Account::class, 'account_id', 'id');
-	}
+    public function account() {
+        return $this->belongsTo(Account::class, 'account_id', 'id');
+    }
 
-	public function accountReport() {
-		return $this->hasOne(AccountReport::class, 'report_id', 'id');
-	}
-	
-public function socialmediaReports() {
-		return $this->hasMany(SocialmediaReport::class,  'report_id', 'id');
-	}
-public function competitorReports() {
-		return $this->hasMany(CompetitorReport::class,  'report_id', 'id');
-	}
-	
-	public function users() {
-		return $this->belongsTo(User::class, 'user_id', 'id');
-	}
+    public function accountReport() {
+        return $this->hasOne(AccountReport::class, 'report_id', 'id');
+    }
 
-	
+    public function socialmediaReports() {
+        return $this->hasMany(SocialmediaReport::class, 'report_id', 'id');
+    }
+
+    public function competitorReports() {
+        return $this->hasMany(CompetitorReport::class, 'report_id', 'id');
+    }
+
+    public function users() {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
 
 }
