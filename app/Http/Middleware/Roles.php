87ef597f4ff951c12602bf247a5f2508c8a90cc9
cobra <cobra@cobra-desktop>
2021-06-17 19:15:25 -0300

    mudanças no middleware para bloquear acesso indevido

--- app/Http/Middleware/Roles.php
+++ app/Http/Middleware/Roles.php
@@ -5,8 +5,8 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Support\Facades\Auth;
 
-class Roles
-{
+class Roles {
+
     /**
      * Handle an incoming request.
      *
@@ -14,38 +14,39 @@ class Roles
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
-    {
-		if(Auth::user() == false){
-			return redirect('login');
-		}
-		elseif(Auth::user()->perfil == 'super administrador') {
-			$role = "superadmin";
+    public function handle($request, Closure $next) {
+        if (Auth::user() == false) {
+            return redirect('login');
+        } elseif (Auth::user()->perfil == 'super administrador') {
+            $role = "superadmin";
 //                        $account->id != auth()->user()->account_id) {
 //            echo "Você não possui permissão para acessar esta página.";
-//            dd($request->route('account'));
+            ;
 //            redirect()->back();
-		}
-		elseif(Auth::user()->perfil == 'dono') {
-			$role = "dono";
-		}
-		elseif(Auth::user()->perfil == 'administrador') {
-			$role = "administrator";
-		}
-		elseif(Auth::user()->perfil == 'funcionario') {
-			$role = "employee";
-		}
-		elseif(Auth::user()->perfil == 'cliente') {
-			$role = "customer";
-		}else{
-			return redirect('painel');
-		}
-		
-		$request->merge([
-                    'role' => $role,
+        } elseif (Auth::user()->perfil == 'dono') {
+            $role = "dono";
+        } elseif (Auth::user()->perfil == 'administrador') {
+            $role = "administrator";
+        } elseif (Auth::user()->perfil == 'funcionario') {
+            $role = "employee";
+        } elseif (Auth::user()->perfil == 'cliente') {
+            $role = "customer";
+        } else {
+            return redirect('painel');
+        }
+
+        if ($request->route('account')) {
+            echo 'nulo';
+        } else {
+            echo 'nao nulo';
+        }
+//                dd($request->route('account'))
+        $request->merge([
+            'role' => $role,
 //                    'account_id' => auth()->user()->account_id,
-                        ]);
-		
+        ]);
+
         return $next($request);
     }
+
 }
