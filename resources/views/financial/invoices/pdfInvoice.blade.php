812a2919a4e80aaff8283d687c0d3790b22b8ff5
cobra <cobra@cobra-desktop>
2021-07-14 09:36:28 -0300

    retirada do relatório de tarefas em faturas

--- resources/views/financial/invoices/pdfInvoice.blade.php
+++ resources/views/financial/invoices/pdfInvoice.blade.php
@@ -256,92 +256,11 @@
             </tr>
             @endif
         </table>
-        <!--QUEBRA 1-->
-        <p class="break"></p>
-
-        @if($data['tasksOperational'])
-        <br>
-        <h4 style="color:{{$data['accountPrincipalColor']}}">
-            EXECUÇÃO:
-        </h4>
-        <p style="text-align: left;margin-top: 0px;">
-            Os serviços contratados nesta fatura representam um total de pontos que foram utilizados como descrito abaixo:
-        </p>
-        <br>
-        <table style="width: 100%">
-            <tr>
-                <td class="table-list-header center" style="width: 15%;background-color:{{$data['accountComplementaryColor']}}">
-                    INÍCIO 
-                </td>
-                <td   class="table-list-header center" style="width: 55%;background-color:{{$data['accountComplementaryColor']}}">
-                    TAREFA
-                </td>
-                <td   class="table-list-header center" style="width: 10%;background-color:{{$data['accountComplementaryColor']}}">
-                    PONTOS
-                </td>
-                <td   class="table-list-header center" style="width: 10%;background-color:{{$data['accountComplementaryColor']}}">
-                    CONCLUSÃO
-                </td>
-                <td   class="table-list-header center" style="width: 10%;background-color:{{$data['accountComplementaryColor']}}">
-                    SITUAÇÃO
-                </td>
-            </tr>
-
-            @foreach ($data['tasksOperational'] as $task)
-            <tr>
-                <td class="table-list center" style="font-color:{{$data['accountComplementaryColor']}}">
-                    {{dateBr($task->date_start)}}
-                </td>
-                <td class="table-list left" style="font-color:{{$data['accountComplementaryColor']}}">
-                    {{$task->name}}
-                </td>
-                <td class="table-list center" style="font-color:{{$data['accountComplementaryColor']}}">
-                    {{$task->points}}
-                </td>
-                <td class="table-list center" style="font-color:{{$data['accountComplementaryColor']}}">
-                    @if($task->date_conclusion)           
-                    {{dateBr($task->date_conclusion)}}
-                    @else
-                    em aberto
-                    @endif
-                </td>
-                @if($task->status == 'fazer')
-                <td class="toDo" style="font-color:{{$data['accountComplementaryColor']}}">
-                @elseif($task->status == 'feito')
-                <td class="done" style="font-color:{{$data['accountComplementaryColor']}}">
-                @endif
-                    {{strtoupper($task->status)}}
-                </td>
-            </tr>
-            <tr>
-                <td class="description left" colspan="5">
-                    {!!html_entity_decode($task->description)!!}
-                </td>
-            </tr>
-            @endforeach
-            <tr>
-                <td   class="table-list-header right" style="font-size: 14px;background-color:{{$data['accountComplementaryColor']}}" colspan="3">
-                    PONTOS CONTRATADOS: 
-                </td>
-                <td   class="table-list-header right" style="font-size: 14px;background-color:{{$data['accountComplementaryColor']}}" colspan="2">
-                    {{$data['tasksOperationalPoints']}}
-                    <br>
-                </td>
-            </tr>
-            <tr>
-                <td   class="table-list-header right"  style="font-size: 14px;background-color:{{$data['accountPrincipalColor']}}" colspan="3">
-                    PONTOS EXECUTADOS: 
-                </td>
-                <td   class="table-list-header right"   style="font-size: 14px;background-color:{{$data['accountPrincipalColor']}}" colspan="2">
-                    {{$data['tasksOperationalPointsExecuted']}}
-                </td>
-            </tr>
-        </table>
-        <br>
-
+  
+        
         <!--QUEBRA 2-->
         <p class="break"></p>
-        @endif
+
 
         <div style='padding-top: 60px;text-align: center'>
             <h4 style="color:{{$data['accountPrincipalColor']}}">
