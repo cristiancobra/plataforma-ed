a5a5ccf038787e643fc9fa888383bd9be87a450c
cobra <cobra@cobra-desktop>
2021-08-24 11:39:29 -0300

    relatorio financeiro

--- app/Console/Commands/CreateExpensesProposalsFromInvoices.php
+++ app/Console/Commands/CreateExpensesProposalsFromInvoices.php
@@ -8,21 +8,21 @@ use App\Models\Opportunity;
 use App\Models\ProductProposal;
 use App\Models\Proposal;
 
-class CreateProposalsFromInvoices extends Command {
+class CreateExpensesProposalsFromInvoices extends Command {
 
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'db:proposalsFromInvoices';
+    protected $signature = 'db:expensesFromInvoices';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Migração do modelo antigo de faturas para propostas com faturas';
+    protected $description = 'Migração do modelo antigo de faturas de débito para propostas com faturas';
 
     /**
      * Create a new command instance.
@@ -39,43 +39,41 @@ class CreateProposalsFromInvoices extends Command {
      * @return int
      */
     public function handle() {
-        $opportunities = Opportunity::where('id', '>', 1)
-                ->with('invoices.invoiceLines')
+        $invoices = Invoice::where('type', 'despesa')
+                ->with('invoiceLines')
                 ->get();
 
-        foreach ($opportunities as $opportunity) {
-            foreach ($opportunity->invoices as $invoice) {
+        foreach ($invoices as $invoice) {
 
-                if ($invoice->number_installment == 1) {
-                    $proposal = new Proposal();
-                    $proposal->identifier = $invoice->identifier;
-                    $proposal->account_id = $invoice->account_id;
-                    $proposal->user_id = $invoice->user_id;
-                    $proposal->opportunity_id = $invoice->opportunity_id;
-                    $proposal->company_id = $invoice->company_id;
-                    $proposal->contact_id = $invoice->contact_id;
-                    $proposal->date_creation = $invoice->date_creation;
-                    $proposal->pay_day = $invoice->pay_day;
-                    $proposal->description = $invoice->description;
-                    $proposal->discount = $invoice->discount;
-                    $proposal->totalHours = $invoice->totalHours;
-                    $proposal->totalPoints = $invoice->totalPoints;
-                    $proposal->totalCost = $invoice->totalCost;
-                    $proposal->totalTax_rate = $invoice->totalTax_rate;
-                    $proposal->totalPrice = $invoice->totalPrice;
-                    $proposal->totalMargin = $invoice->totalMargin;
-                    $proposal->totalBalance = $invoice->totalBalance;
-                    $proposal->receipt = $invoice->receipt;
-                    $proposal->installment = $invoice->number_installment_total;
-                    $proposal->type = $invoice->type;
-                    $proposal->status = $invoice->status;
-                    $proposal->created_at = $invoice->created_at;
-                    $proposal->updated_at = $invoice->updated_at;
-                    $proposal->expiration_date = $invoice->expiration_date;
-                    $proposal->save();
-                }
+            if ($invoice->number_installment == 1) {
+                $proposal = new Proposal();
+                $proposal->identifier = $invoice->identifier;
+                $proposal->account_id = $invoice->account_id;
+                $proposal->user_id = $invoice->user_id;
+//                $proposal->opportunity_id = $invoice->opportunity_id;
+                $proposal->company_id = $invoice->company_id;
+                $proposal->contact_id = $invoice->contact_id;
+                $proposal->date_creation = $invoice->date_creation;
+                $proposal->pay_day = $invoice->pay_day;
+                $proposal->description = $invoice->description;
+                $proposal->discount = $invoice->discount;
+                $proposal->totalHours = $invoice->totalHours;
+                $proposal->totalPoints = $invoice->totalPoints;
+                $proposal->totalCost = $invoice->totalCost;
+                $proposal->totalTax_rate = $invoice->totalTax_rate;
+                $proposal->totalPrice = $invoice->totalPrice;
+                $proposal->totalMargin = $invoice->totalMargin;
+                $proposal->totalBalance = $invoice->totalBalance;
+                $proposal->receipt = $invoice->receipt;
+                $proposal->installment = $invoice->number_installment_total;
+                $proposal->type = $invoice->type;
+                $proposal->status = $invoice->status;
+                $proposal->created_at = $invoice->created_at;
+                $proposal->updated_at = $invoice->updated_at;
+                $proposal->expiration_date = $invoice->expiration_date;
+                $proposal->save();
+
                 $invoice->proposal_id = $proposal->id;
-//                dd($invoice);
                 $invoice->save();
 
                 foreach ($invoice->invoiceLines as $invoiceLine) {
@@ -92,7 +90,6 @@ class CreateProposalsFromInvoices extends Command {
                     $productProposal->created_at = $invoiceLine->created_at;
                     $productProposal->updated_at = $invoiceLine->updated_at;
                     $productProposal->save();
-                    
                 }
             }
         }
--- app/Console/Commands/CreateProposalsFromInvoices.php
+++ app/Console/Commands/CreateProposalsFromInvoices.php
@@ -8,21 +8,21 @@ use App\Models\Opportunity;
 use App\Models\ProductProposal;
 use App\Models\Proposal;
 
-class CreateExpensesProposalsFromInvoices extends Command {
+class CreateProposalsFromInvoices extends Command {
 
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'db:expensesFromInvoices';
+    protected $signature = 'db:proposalsFromInvoices';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Migração do modelo antigo de faturas de débito para propostas com faturas';
+    protected $description = 'Migração do modelo antigo de faturas para propostas com faturas';
 
     /**
      * Create a new command instance.
@@ -39,41 +39,43 @@ class CreateExpensesProposalsFromInvoices extends Command {
      * @return int
      */
     public function handle() {
-        $invoices = Invoice::where('type', 'despesa')
-                ->with('invoiceLines')
+        $opportunities = Opportunity::where('id', '>', 1)
+                ->with('invoices.invoiceLines')
                 ->get();
 
-        foreach ($invoices as $invoice) {
+        foreach ($opportunities as $opportunity) {
+            foreach ($opportunity->invoices as $invoice) {
 
-            if ($invoice->number_installment == 1) {
-                $proposal = new Proposal();
-                $proposal->identifier = $invoice->identifier;
-                $proposal->account_id = $invoice->account_id;
-                $proposal->user_id = $invoice->user_id;
-//                $proposal->opportunity_id = $invoice->opportunity_id;
-                $proposal->company_id = $invoice->company_id;
-                $proposal->contact_id = $invoice->contact_id;
-                $proposal->date_creation = $invoice->date_creation;
-                $proposal->pay_day = $invoice->pay_day;
-                $proposal->description = $invoice->description;
-                $proposal->discount = $invoice->discount;
-                $proposal->totalHours = $invoice->totalHours;
-                $proposal->totalPoints = $invoice->totalPoints;
-                $proposal->totalCost = $invoice->totalCost;
-                $proposal->totalTax_rate = $invoice->totalTax_rate;
-                $proposal->totalPrice = $invoice->totalPrice;
-                $proposal->totalMargin = $invoice->totalMargin;
-                $proposal->totalBalance = $invoice->totalBalance;
-                $proposal->receipt = $invoice->receipt;
-                $proposal->installment = $invoice->number_installment_total;
-                $proposal->type = $invoice->type;
-                $proposal->status = $invoice->status;
-                $proposal->created_at = $invoice->created_at;
-                $proposal->updated_at = $invoice->updated_at;
-                $proposal->expiration_date = $invoice->expiration_date;
-                $proposal->save();
-
+                if ($invoice->number_installment == 1) {
+                    $proposal = new Proposal();
+                    $proposal->identifier = $invoice->identifier;
+                    $proposal->account_id = $invoice->account_id;
+                    $proposal->user_id = $invoice->user_id;
+                    $proposal->opportunity_id = $invoice->opportunity_id;
+                    $proposal->company_id = $invoice->company_id;
+                    $proposal->contact_id = $invoice->contact_id;
+                    $proposal->date_creation = $invoice->date_creation;
+                    $proposal->pay_day = $invoice->pay_day;
+                    $proposal->description = $invoice->description;
+                    $proposal->discount = $invoice->discount;
+                    $proposal->totalHours = $invoice->totalHours;
+                    $proposal->totalPoints = $invoice->totalPoints;
+                    $proposal->totalCost = $invoice->totalCost;
+                    $proposal->totalTax_rate = $invoice->totalTax_rate;
+                    $proposal->totalPrice = $invoice->totalPrice;
+                    $proposal->totalMargin = $invoice->totalMargin;
+                    $proposal->totalBalance = $invoice->totalBalance;
+                    $proposal->receipt = $invoice->receipt;
+                    $proposal->installment = $invoice->number_installment_total;
+                    $proposal->type = $invoice->type;
+                    $proposal->status = $invoice->status;
+                    $proposal->created_at = $invoice->created_at;
+                    $proposal->updated_at = $invoice->updated_at;
+                    $proposal->expiration_date = $invoice->expiration_date;
+                    $proposal->save();
+                }
                 $invoice->proposal_id = $proposal->id;
+//                dd($invoice);
                 $invoice->save();
 
                 foreach ($invoice->invoiceLines as $invoiceLine) {
@@ -90,6 +92,7 @@ class CreateExpensesProposalsFromInvoices extends Command {
                     $productProposal->created_at = $invoiceLine->created_at;
                     $productProposal->updated_at = $invoiceLine->updated_at;
                     $productProposal->save();
+                    
                 }
             }
         }
