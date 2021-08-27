01362a11d17622afed96fdb9fd861360005bc095
cobra <cobra@cobra-desktop>
2021-08-27 18:11:09 -0300

    relatorio financeiro

--- resources/views/financial/invoices/reportPdf.blade.php
+++ resources/views/financial/invoices/reportPdf.blade.php
@@ -5,12 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title></title>
-
-                    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <!-- Styles -->
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
         
     </head>
     <body>
