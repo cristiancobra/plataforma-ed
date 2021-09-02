802337ffbe37e2ec1ac063ccd4410d676582118d
cobra <cobra@cobra-desktop>
2021-09-02 16:36:53 -0300

    relatorio financeiro

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -124,7 +124,7 @@ class Page extends Model {
     public static function formFields($page) {
         $formFields = [];
         $counter = 1;
-
+dd($page->getAttributes());
         foreach ($page->getAttributes() as $name => $value) {
             switch ($name) {
                 case('contact_first_name'):
