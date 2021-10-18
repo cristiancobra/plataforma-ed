8c43f01e84fd561333447c10f6a16fa538236968
cobra <cobra@cobra-desktop>
2021-10-18 16:06:33 -0300

    correção: criar movimentacao de saida nao salva

--- resources/views/financial/invoices/indexInvoices.blade.php
+++ resources/views/financial/invoices/indexInvoices.blade.php
@@ -1,9 +1,26 @@
-@extends('layouts/master')
+@extends('layouts/index')
 
 @section('title','FATURAS')
 
 @section('image-top')
 {{asset('images/invoice.png')}} 
+@endsection
+
+@section('filter')
+<form id="filter" action="{{route('invoice.index')}}" method="get" style="text-align: right;display:none">
+    <input type="text" name="name" placeholder="nome da oportunidade" value="">
+    <input type="date" name="date_start" size="20" value="{{old('date_start')}}"><span class="fields"></span>
+    <input type="date" name="date_end" size="20" value="{{old('date_end')}}"><span class="fields"></span>
+    {{createFilterSelectModels('company_id', 'select', $companies, 'Todas as empresas')}}
+    {{createFilterSelectModels('contact_id', 'select', $contacts, 'Todas os contatos')}}
+    {{createFilterSelect('status', 'select', returnInvoiceStatusToFilter(), 'Todas as situações')}}
+    {{returnType('type', 'select', 'invoice')}}
+    <br>
+    <a class="text-button secondary" href='{{route('invoice.index')}}'>
+        LIMPAR
+    </a>
+    <input class="text-button primary" type="submit" value="FILTRAR">
+</form>
 @endsection
 
 @section('buttons')
@@ -23,69 +40,71 @@
 {{createButtonList('invoice')}}
 @endsection
 
-@section('main')
-<div class='row'>
-    <div class="tb tb-header-start offset-6 col-2">
+@section('shortcuts')
+<div class='col-2 offset-4 d-inline-block tasks-my'>
+    <a style='text-decoration:none' href='{{route('invoice.index', [
+				'date_start' => $monthStart,
+				'date_end' => $monthEnd,
+				])}}'>
+                            <p class='panel-text' style="font-size: 20px">
+            MÊS
+            <br>
+            ATUAL
+        </p>
+    </a>
+</div>
+<div class='col-6'>
+<div class='row mt-2'>
+    <div class="tb tb-header-start col">
         PREVISÃO
     </div>
-    <div   class="tb tb-header col-2">
+    <div   class="tb tb-header col">
         MÊS
     </div>
-    <div   class="tb tb-header-end col-2">
+    <div   class="tb tb-header-end col">
         ANO
     </div>
 </div>
 <div class='row'>
-    <div class="tb offset-6 col-2 justify-content-start">
+    <div class="tb col justify-content-start">
         RECEITAS:
     </div>
-    <div class="tb col-2 justify-content-end">
+    <div class="tb col justify-content-end">
         + {{formatCurrencyReal($estimatedRevenueMonthly)}}
     </div>
-    <div class="tb col-2 justify-content-end">
+    <div class="tb col justify-content-end">
         + {{formatCurrencyReal($estimatedRevenueYearly)}}
     </div>
 </div>
 <div class='row'>
-    <div class="tb offset-6 col-2 justify-content-start">
+    <div class="tb col justify-content-start">
         DESPESAS:
     </div>
-    <div class="tb col-2 justify-content-end">
+    <div class="tb col justify-content-end">
         - {{formatCurrencyReal($estimatedExpenseMonthly)}}
     </div>
-    <div class="tb col-2 justify-content-end">
+    <div class="tb col justify-content-end">
         - {{formatCurrencyReal($estimatedExpenseYearly)}}
     </div>
 </div>
 <div class='row'>
-    <div class="tb offset-6 col-2 justify-content-start">
+    <div class="tb col justify-content-start">
         SALDO:
     </div>
-    <div class="tb col-2 justify-content-end">
+    <div class="tb col justify-content-end">
         {{formatCurrencyReal($estimatedRevenueMonthly - $estimatedExpenseMonthly)}}
     </div>
-    <div class="tb col-2 justify-content-end">
+    <div class="tb col justify-content-end">
         {{formatCurrencyReal($estimatedRevenueYearly - $estimatedExpenseYearly)}}
     </div>
 </div>
-<br>
-<br>
-<form id="filter" action="{{route('invoice.index')}}" method="get" style="text-align: right;display:none">
-    <input type="text" name="name" placeholder="nome da oportunidade" value="">
-    <input type="date" name="date_start" size="20" value="{{old('date_start')}}"><span class="fields"></span>
-    <input type="date" name="date_end" size="20" value="{{old('date_end')}}"><span class="fields"></span>
-    {{createFilterSelectModels('company_id', 'select', $companies, 'Todas as empresas')}}
-    {{createFilterSelectModels('contact_id', 'select', $contacts, 'Todas os contatos')}}
-    {{createFilterSelect('status', 'select', returnInvoiceStatusToFilter(), 'Todas as situações')}}
-    {{returnType('type', 'select', 'invoice')}}
-    <br>
-    <a class="text-button secondary" href='{{route('invoice.index')}}'>
-        LIMPAR
-    </a>
-    <input class="text-button primary" type="submit" value="FILTRAR">
-</form>
+</div>
+@endsection
+
+
+@section('table')
 <div>
-    <div class='row'>
+    <div class='row mt-2'>
         <div   class="tb tb-header-start col-1">
             ID
         </div>
@@ -101,8 +120,11 @@
         <div   class="tb tb-header col-1">
             VENCIMENTO
         </div>
-        <div   class="tb tb-header col-2">
-            VALOR
+        <div   class="tb tb-header col-1">
+            TOTAL
+        </div>
+        <div   class="tb tb-header col-1">
+            SALDO
         </div>
         <div   class="tb tb-header-end col-1">
             SITUAÇÃO
@@ -149,13 +171,24 @@
             {{date('d/m/Y', strtotime($invoice->pay_day))}}
         </div>
         @endif
-        @if($invoice->type == 'receita')
-        <div class="tb col-2 justify-content-end" style="text-align: right">
+        
+        @if($invoice->totalPrice >= 0)
+        <div class="tb col-1 justify-content-end" style="text-align: right">
             {{formatCurrencyReal($invoice->totalPrice)}}
         </div>
         @else
-        <div class="tb col-2 justify-content-end" style="color: red;text-align: right">
+        <div class="tb col-1 justify-content-end" style="color: red;text-align: right">
             {{formatCurrencyReal($invoice->totalPrice)}}
+        </div>
+        @endif
+        
+        @if($invoice->totalPrice >= 0)
+        <div class="tb col-1 justify-content-end" style="text-align: right">
+            {{formatCurrencyReal($invoice->balance)}}
+        </div>
+        @else
+        <div class="tb col-1 justify-content-end" style="color: red;text-align: right">
+            {{formatCurrencyReal($invoice->balance)}}
         </div>
         @endif
 
