186725b046bee007ca8b1c30cdeeefe529fc2a22
cobra <cobra@cobra-desktop>
2021-10-25 12:28:52 -0300

    atualização: loja

--- resources/views/administrative/users/show.blade.php
+++ resources/views/administrative/users/show.blade.php
@@ -54,26 +54,88 @@
     </div>
 </div>
 
-<div style='text-align: right'>
-    <div style='text-align:right;color: #874983; display: inline-block'>
-        <form action='{{ route('user.destroy', ['user' => $user->id]) }}' method='post'>
-            @csrf
-            @method('delete')
-            <button id='' class='circular-button delete' style='border:none;padding-left:7px;padding-top: -2px' "type='submit'>
-                <i class='fa fa-trash'></i>
-            </button>
-        </form>
+<div class='row mt-5'>
+    <div class='tb tb-header-start col-3'>
+        TAREFAS
     </div>
-    <div style='text-align:right;color: #874983;display: inline-block'>
-        <a class='circular-button secondary' href=' {{ route('user.edit', ['user' => $user->id]) }}'>
-            <i class='fa fa-edit'></i>
+    <div class='tb tb-header col-3'>
+        CONTATO
+    </div>
+    <div class='tb tb-header col-3'>
+        EMPRESA
+    </div>
+    <div class='tb tb-header col-1'>
+        PRAZO
+    </div>
+    <div class='tb tb-header col-1'>
+        PRIORIDADE
+    </div>
+    <div class='tb tb-header-end col-1'>
+        SITUAÇÃO
+    </div>
+</div>
+@foreach ($userTasks as $task)
+<div class='row'>
+    <div class='tb col-3 justify-content-start' style="font-weight: 600">
+        <a class="white" href=" {{ route('task.show', ['task' => $task->id]) }}">
+            <button class="button-round">
+                <i class='fa fa-eye'></i>
+            </button>
+        </a>
+        {{$task->name}}
+    </div>
+    <div class='tb col-3'>
+        <a  class="white" href=" {{ route('contact.show', ['contact' => $task->contact_id]) }}">
+            @if(isset($task->contact->name))
+            {{$task->contact->name}}
+            @else
+            contato excluído
+            @endif
         </a>
     </div>
-    <a class='circular-button primary'  href='{{route('user.index')}}'>
-        <i class='fas fa-arrow-left'></i>
-    </a>
+    <div class='tb col-3'>
+        @if(isset($task->company->name))
+        {{$task->company->name}}
+        @else
+        não possui
+        @endif
+    </div>
+
+    {{formatDateDue($task)}}
+
+    {{formatPriority($task)}}
+
+    {{formatStatus($task)}}
+
 </div>
-<br>
+@endforeach
+
+<div class='row mt-5 mb-5'>
+    <div class="col-sm-8 col-xs-12" style="font-size: 18px">
+        {{$userTasks->links()}}
+    </div>
+    <div class="col-sm-4 col-xs-12 d-inline-block" style="text-align: right">
+        <div style='text-align:right;color: #874983; display: inline-block'>
+            <form action='{{ route('user.destroy', ['user' => $user->id]) }}' method='post'>
+                @csrf
+                @method('delete')
+                <button id='' class='circular-button delete' style='border:none;padding-left:7px;padding-top: -2px' "type='submit'>
+                    <i class='fa fa-trash'></i>
+                </button>
+            </form>
+        </div>
+        <div style='text-align:right;color: #874983;display: inline-block'>
+            <a class='circular-button secondary' href=' {{ route('user.edit', ['user' => $user->id]) }}'>
+                <i class='fa fa-edit'></i>
+            </a>
+        </div>
+        <a class='circular-button primary'  href='{{route('user.index')}}'>
+            <i class='fas fa-arrow-left'></i>
+        </a>
+    </div>
+</div>
+
+
 <p style='text-align: left;margin-left: 30px;color: white;font-size: 14px'>* se a <b>senha padrão</b> tiver sido alterada pelo usuário, atualize a senha novamente com a <b>senha padrão</b>. Peça para o usuário alterar sua senha no seu primeiro acesso.</p>
 </div>
 
