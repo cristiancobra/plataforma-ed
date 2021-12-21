ad762fd81f07451f10c6c7664e3dfcc3bdaad6b1
cobra <cobra@cobra-desktop>
2021-12-21 20:25:28 -0300

    correção em adicionar pagamento em oportunidades

--- resources/js/general.js
+++ resources/js/general.js
@@ -13,48 +13,43 @@ function buttonFilter() {
         });
 
     });
-};
+}
+;
 
 
-window.formatCurrencyReal = function(
+window.formatCurrencyReal = function (
         fieldId
         ) {
 
-        var elemento = document.getElementById(fieldId);
-        var valor = elemento.value;
-        
+    var elemento = document.getElementById(fieldId);
+    var valor = elemento.value;
 
-        valor = valor + '';
-        valor = parseInt(valor.replace(/[\D]+/g, ''));
-        valor = valor + '';
-        valor = valor.replace(/([0-9]{2})$/g, ",$1");
 
-        if (valor.length > 6) {
-            valor = valor.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
-        }
+    valor = valor + '';
+    valor = parseInt(valor.replace(/[\D]+/g, ''));
+    valor = valor + '';
+    valor = valor.replace(/([0-9]{2})$/g, ",$1");
 
-        elemento.value = valor;
-        if(valor == 'NaN') elemento.value = '';
-        
-    };
-    
-window.formatCurrencyRealAll = function(
+    if (valor.length > 6) {
+        valor = valor.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
+    }
+
+    elemento.value = valor;
+    if (valor == 'NaN')
+        elemento.value = '';
+
+};
+
+window.formatCurrencyRealAll = function (
         inputClass
         ) {
 
-        var elements = document.querySelectorAll(inputClass);
+    var elements = document.querySelectorAll(inputClass);
 
-//const highlightedItems = userList.querySelectorAll(".highlighted");
+    elements.forEach(function (elemento) {
 
-//elements.forEach(function(userItem) {
-//  deleteUser(userItem);
-//});
-
-elements.forEach(function(elemento) {
-    
-//    var elemento = document.getElementById(fieldId);
         var valor = elemento.value;
-        
+
 
         valor = valor + '';
         valor = parseInt(valor.replace(/[\D]+/g, ''));
@@ -66,35 +61,45 @@ elements.forEach(function(elemento) {
         }
 
         elemento.value = valor;
-        if(valor == 'NaN') elemento.value = '';
-        
-});
-    };
+        if (valor == 'NaN')
+            elemento.value = '';
 
-//        var valor = elemento.value;
-//        
-//
-//        valor = valor + '';
-//        valor = parseInt(valor.replace(/[\D]+/g, ''));
-//        valor = valor + '';
-//        valor = valor.replace(/([0-9]{2})$/g, ",$1");
-//
-//        if (valor.length > 6) {
-//            valor = valor.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
-//        }
-//
-//        elemento.value = valor;
-//        if(valor == 'NaN') elemento.value = '';
-//        
-//    };
+    });
+};
+
+window.buttonOpenBox = function (
+        inputClass
+        ) {
+
+    var elements = document.querySelectorAll(inputClass);
+
+    elements.forEach(function (elemento) {
+
+        var valor = elemento.value;
 
 
-window.loadProjectStagesJson = function(
-	changeId,
-	targetId,
-	url,
-	targetOptionSelected
-                ) {
+        valor = valor + '';
+        valor = parseInt(valor.replace(/[\D]+/g, ''));
+        valor = valor + '';
+        valor = valor.replace(/([0-9]{2})$/g, ",$1");
+
+        if (valor.length > 6) {
+            valor = valor.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
+        }
+
+        elemento.value = valor;
+        if (valor == 'NaN')
+            elemento.value = '';
+
+    });
+};
+
+window.loadProjectStagesJson = function (
+        changeId,
+        targetId,
+        url,
+        targetOptionSelected
+        ) {
 //    function loadRoteirosSimuladosOrderListJson(
 //	changeId,
 //	targetId,
@@ -102,59 +107,57 @@ window.loadProjectStagesJson = function(
 //	targetOptionSelected
 //) {
 //alert('teste');
-	var changeableElement = document.getElementById(changeId);
-	var targetElement = document.getElementById(targetId);
-	
-	var removeOptions = function() {
-		// remove options except empty (first, 0)
-		while (targetElement.options.length > 1) {
-			targetElement.remove(1);
-		}
-	};
-	
-	var runXhrRequest = function() {
-		if(changeableElement.value) {
-			var xhr = new XMLHttpRequest();
+    var changeableElement = document.getElementById(changeId);
+    var targetElement = document.getElementById(targetId);
 
-			xhr.responseType = 'json';
-			xhr.open('GET', url + '/' + changeableElement.value, true);
-			xhr.onload = function() {
-				removeOptions();
-				
-				// add options to select element
-				for(var index in xhr.response) {
-                                    console.log(xhr.response[index]);
-					var newOption = new Option(
-						xhr.response[index].name,
-						xhr.response[index].id
-					);
-					targetElement.add(newOption);
-				}
-				
-				if(targetOptionSelected) {
-					if (
-						targetElement.querySelector('[value="' + targetOptionSelected + '"]')
-					) {
-						targetElement.value = targetOptionSelected;
-					}
-					else {
-						targetElement.value = '';
-					}
-					
-					// select only first time
-					targetOptionSelected = false;
-				}
-			};
-			xhr.send();
-		}
-		else {
-			removeOptions();
-		}
-	};
-	
-	runXhrRequest();
-	
-	changeableElement.onchange = runXhrRequest;
+    var removeOptions = function () {
+        // remove options except empty (first, 0)
+        while (targetElement.options.length > 1) {
+            targetElement.remove(1);
+        }
+    };
+
+    var runXhrRequest = function () {
+        if (changeableElement.value) {
+            var xhr = new XMLHttpRequest();
+
+            xhr.responseType = 'json';
+            xhr.open('GET', url + '/' + changeableElement.value, true);
+            xhr.onload = function () {
+                removeOptions();
+
+                // add options to select element
+                for (var index in xhr.response) {
+                    console.log(xhr.response[index]);
+                    var newOption = new Option(
+                            xhr.response[index].name,
+                            xhr.response[index].id
+                            );
+                    targetElement.add(newOption);
+                }
+
+                if (targetOptionSelected) {
+                    if (
+                            targetElement.querySelector('[value="' + targetOptionSelected + '"]')
+                            ) {
+                        targetElement.value = targetOptionSelected;
+                    } else {
+                        targetElement.value = '';
+                    }
+
+                    // select only first time
+                    targetOptionSelected = false;
+                }
+            };
+            xhr.send();
+        } else {
+            removeOptions();
+        }
+    };
+
+    runXhrRequest();
+
+    changeableElement.onchange = runXhrRequest;
 };
 
         
